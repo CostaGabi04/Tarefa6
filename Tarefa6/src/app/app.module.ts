e9d1d1939d71f97b14b3d142a711da922e0610b8
Gabriele Costa <itsgacosta.ads@gmail.com>
2023-11-14 22:24:27 -0300

    ProjetoFinal

---
+++ Tarefa6/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MenuSuperiorComponent } from './menu-superior/menu-superior.component';
+import { CalculadoraComponent } from './calculadora/calculadora.component';
+import { FormsModule } from '@angular/forms';
+import { CalcularMediaComponent } from './calcular-media/calcular-media.component';
+import { TelaPaiComponent } from './tela-pai/tela-pai.component';
+import { TelaFilho1Component } from './tela-pai/tela-filho1/tela-filho1.component';
+import { TelaFilho2Component } from './tela-pai/tela-filho2/tela-filho2.component';
+import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
+import { HomeComponent } from './home/home.component';
+import { UsuarioComponent } from './usuario/usuario.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { UsuarioDetalheComponent } from './usuario/usuario-detalhe/usuario-detalhe.component';
+
+@NgModule({
+  declarations: [AppComponent, MenuSuperiorComponent, CalculadoraComponent, CalcularMediaComponent, TelaPaiComponent, TelaFilho1Component, TelaFilho2Component, PaginaNaoEncontradaComponent, HomeComponent, UsuarioComponent, UsuarioDetalheComponent],
+  imports: [BrowserModule, AppRoutingModule, NgbModule, FormsModule, HttpClientModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ Tarefa6/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  urlApi: 'https://654ac15f5b38a59f28ee3487.mockapi.io/api/v1/'
+};
